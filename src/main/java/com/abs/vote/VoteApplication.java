b4d6eb5eb806a5606edee7f936595e31074e7f54
digi-ranji <ranjith.tr@gmail.com>
2020-03-15 23:27:38 +0800

    From Mac Eclipse 11:11

--- src/main/java/com/abs/vote/VoteApplication.java
+++ src/main/java/com/abs/vote/VoteApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
+
 import redis.clients.jedis.Jedis; 
 
 @SpringBootApplication
